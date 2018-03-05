e5b0d4c6ac12cf010ac0bcfd4cd2c75c94837f70
chivagarg <chivagarg@gmail.com>
2018-03-04 22:45:43 -0800

    properties RPC

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,5 +17,14 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping("/properties")
+    public Properties properties(@RequestParam(value="name", defaultValue="World") String name) {
+        List<String> list =  new ArrayList<String>();
+        list.add("2 Townsend st, San Francisco");
+        list.add("2410 East Lynn st, Seattle");
+
+        return new Properties(counter.incrementAndGet(), list);
     }
 }
